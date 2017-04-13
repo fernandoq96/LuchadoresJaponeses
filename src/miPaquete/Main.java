6028fc3a08cd7f47934d9812bc19bd4f54bf96a8
Fernando Queirolo <queirolo.fer@gmail.com>
2017-04-13 10:24:20 -0300

    Agrego los archivos
    AGREGUE UNA LINEA

---
+++ src/miPaquete/Main.java
@@ -0,0 +1,28 @@
+package miPaquete;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+
+	// MODIFICO UNA LINEA OLOLO 
+	public static void main(String[] args) {
+		Torneo championLeague = new Torneo();
+		File archivo = new File("in.txt");
+		try {
+			championLeague.crearTorneo(archivo);
+			championLeague.mostrarTorneo();
+			championLeague.crearResultados();
+			
+			championLeague.imprimirResultados();
+		} catch (FileNotFoundException e) {
+			System.out.println("No se encuentra el archivo");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
